ce0a47266817bb9474ac0ae59985e44c115673a7
Sabrina Marques <sabrinamarques0240@gmail.com>
2025-01-17 17:53:25 -0300

    Adding user screen and responsiveness

--- taqtile/src/pages/Users/Users.tsx
+++ taqtile/src/pages/Users/Users.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from 'react'
+import './Users.css'
 
-const Users = () => {
-  return <h1>Hello World!</h1>;
-};
+function Users() {
+
+  const users = [
+    { id: 1, username: 'João', email: 'joao@gmail.com' },
+    { id: 2, username: 'Pedro', email: 'pedro@gmail.com' },
+    { id: 3, username: 'Joana', email: 'joana@gmail.com' },
+    { id: 4, username: 'Pietra', email: 'pietra@gmail.com' }
+  ];
+
+  return (
+    <div className="users-container">
+    <h1>Lista de Usuários</h1>
+    <div className="cards-container">
+      {users.map((user) => (
+        <div className="card" key={user.id}>
+          <h2 className="card-title">{user.username}</h2>
+          <p><strong>Email:</strong> {user.email}</p>
+        </div>
+      ))}
+    </div>
+  </div>
+  );
+}
 
 export default Users;
